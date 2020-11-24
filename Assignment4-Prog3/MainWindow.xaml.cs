9235dac27cc8d7e6174c1c1fd639d2e8d12ee721
Nateghi <kobra.nateghi@ontario.ca>
2020-11-24 07:56:17 -0500

    added partA&B

--- Assignment4-Prog3/MainWindow.xaml.cs
+++ Assignment4-Prog3/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace Assignment4_Prog3
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rand = new Random();
+        string[] ascii = "! \" # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \\ ] ^ _ ' a b c d e f g h i j k l m n o p q r s t u v w x y z ( | ) ~".Split(' ');
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int asciiCount = ascii.Length;
+            int nubmerOfWords = int.Parse(NumberOfWords.Text);
+            StringBuilder sb = new StringBuilder();
+            for (int i = nubmerOfWords - 1; i >= 0; i--)
+            {
+                int charactersCount = rand.Next(20);
+                string word = "";
+                for (int j = charactersCount; j >= 0; j--)
+                {
+                    word += ascii[rand.Next(asciiCount)];
+                }
+
+                sb.Append($"{word} ");
+            }
+            Result.Text = sb.ToString();
         }
     }
 }
