c0f127762f5c91e0a269942ea6446f725897b0d4
Otaku2932 <c_lapis4@hotmail.com>
2020-11-25 18:27:07 -0500

    Q1A finished

---
+++ Question1A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1A
+{
+    class Program
+    {
+        static int Search(string[] arrayToSearch, string searchKey)
+        {
+            int iteration = 0;
+            foreach (string search in arrayToSearch)
+            {
+                if (searchKey == search)
+                    return iteration;
+                iteration++;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            string[] wordList = { "okay", "yes", "maybe", "but", "no" };
+            Console.WriteLine("location of 'no' in wordlist is " + Search(wordList, "no"));
+            Console.WriteLine("when location is not found in word list it is " + Search(wordList, "ayaya"));
+        }
+    }
+}
