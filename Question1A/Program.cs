c3fcf4a6ef261b049a4dc960c8a424a21e73bc88
Giang Nguyen <giangkawaii1999@gmail.com>
2020-11-25 18:52:56 -0500

    quesiton 1A generic method

--- Question1A/Program.cs
+++ Question1A/Program.cs
@@ -8,22 +8,33 @@ namespace Question1A
 {
     class Program
     {
-        static int Search(string[] arrayToSearch, string searchKey)
+        //static int Search(string[] arrayToSearch, string searchKey)
+        //{
+        //    int iteration = 0;
+        //    foreach (string search in arrayToSearch)
+        //    {
+        //        if (searchKey == search)
+        //            return iteration;
+        //        iteration++;
+        //    }
+        //    return -1;
+        //}
+        static int Search<T>(T[] arrayToSearch, T searchKey)
         {
-            int iteration = 0;
-            foreach (string search in arrayToSearch)
-            {
-                if (searchKey == search)
-                    return iteration;
-                iteration++;
-            }
-            return -1;
+            return Array.IndexOf(arrayToSearch, searchKey);
         }
         static void Main(string[] args)
         {
             string[] wordList = { "okay", "yes", "maybe", "but", "no" };
             Console.WriteLine("location of 'no' in wordlist is " + Search(wordList, "no"));
             Console.WriteLine("when location is not found in word list it is " + Search(wordList, "ayaya"));
+
+            double[] wordList2 = { 1.2, 2.3, 3.4, 4.5, 6 };
+            Console.WriteLine("location of 'no' in wordlist is " + Search(wordList2, 1.2));
+            Console.WriteLine("when location is not found in word list it is " + Search(wordList2, 12));
+
+
+            Console.ReadKey();
         }
     }
 }
